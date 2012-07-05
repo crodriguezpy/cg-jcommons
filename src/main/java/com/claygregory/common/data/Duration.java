8622cc82d37c51bd4cb05efc441fe6b0797ba7d0
Clay Gregory <gregoryc@perma-bound.com>
2012-07-04 19:44:41 -0500

    refinement of Duration value object

--- src/main/java/com/claygregory/common/data/Duration.java
+++ src/main/java/com/claygregory/common/data/Duration.java
@@ -19,22 +19,22 @@ public class Duration implements Comparable<Duration> {
 	private long duration;
 
 	public Duration( int seconds ) {
-		this.duration = seconds * 1000;
+		this.duration = seconds * MS_IN_SECONDS;
 	}
 
 	public Duration( long ms ) {
 		this.duration = ms;
 	}
 
-	public Duration( Integer minutes, Integer seconds ) {
-		this.duration += ( minutes != null ? minutes : 0 ) * MS_IN_MINUTES;
-		this.duration += ( seconds != null ? seconds : 0 ) * MS_IN_SECONDS;
+	public Duration( int minutes, int seconds ) {
+		this.duration += minutes * MS_IN_MINUTES;
+		this.duration += seconds * MS_IN_SECONDS;
 	}
 
-	public Duration( Integer hours, Integer minutes, Integer seconds ) {
-		this.duration += ( hours != null ? hours : 0 ) * MS_IN_HOURS;
-		this.duration += ( minutes != null ? minutes : 0 ) * MS_IN_MINUTES;
-		this.duration += ( seconds != null ? seconds : 0 ) * MS_IN_SECONDS;
+	public Duration( int hours, int minutes, int seconds ) {
+		this.duration += hours * MS_IN_HOURS;
+		this.duration += minutes * MS_IN_MINUTES;
+		this.duration += seconds * MS_IN_SECONDS;
 	}
 
 	public double asDays( ) {
@@ -56,10 +56,20 @@ public class Duration implements Comparable<Duration> {
 	public double asSeconds( ) {
 		return this.duration / ( double ) MS_IN_SECONDS;
 	}
+	
+	public Duration add( Duration duration ) {
+		return new Duration( this.asMilliseconds( ) + duration.asMilliseconds( ) );
+	}
+	
+	public Duration subtract( Duration duration ) {
+		return new Duration( this.asMilliseconds( ) - duration.asMilliseconds( ) );
+	}
 
 	@Override
 	public String toString( ) {
-		return String.format( "%d:%02d:%02d", ( int ) Math.floor( this.asHours( ) ), ( int ) Math.floor( this.asMinutes( ) ) % 60,
+		return String.format( "%d:%02d:%02d",
+				( int ) Math.floor( this.asHours( ) ),
+				( int ) Math.floor( this.asMinutes( ) ) % 60,
 				( int ) Math.floor( this.asSeconds( ) ) % 60 );
 	}
 
